80eff8899f1a60eac7f6f4e836e91ef504673612
James <radjame52004@gmail.com>
2024-10-23 13:43:02 -0400

    finish /calc/

--- calc/script.js
+++ calc/script.js
@@ -6,16 +6,36 @@ var historyIndex = 0;
 Array.from(document.getElementsByTagName("button")).forEach((element) => {
     console.log("registering buttons")
     element.addEventListener("click", (event) => {
+        // set the random color of the button
+        var randColor = getRandomColor()
+        event.target.style = "background-color: " + randColor;
 
+        // add the keystroke to the history list
+        if (event.target.id != "historyButton"){
+            var ptag = document.createElement("p")
+            ptag.textContent = event.target.id
+            document.getElementById("offcanvasBody").appendChild(ptag)
+        }
+        
         // clear everything and return
         if (event.target.id === "ce" ){
             problem = "";
             document.getElementById("problem").innerText = "Problem: " + problem
+
+            // reset the colors of all of the buttons
+            for(var btn of document.getElementsByTagName("button")){
+                console.log(btn)
+                btn.style = "";
+            }
             return;
         }
         // Calculate the problem
         else if (event.target.id === "="){
-            calcProblem();
+            var solution = calcProblem();
+            var ptag = document.createElement("p")
+            ptag.textContent = solution
+            document.getElementById("offcanvasBody").appendChild(ptag)
+
             return;
         } 
         // Do the history button thing
@@ -31,12 +51,17 @@ Array.from(document.getElementsByTagName("button")).forEach((element) => {
     })
 });
 
+function getRandomColor(){
+    return "rgb(" + Math.trunc(Math.random() * 255) + ", " + Math.trunc(Math.random() * 255) + ", " + Math.trunc(Math.random() * 255) + ");";
+}
+
 function calcProblem(){
     try {
         var solution = eval(problem)
         document.getElementById("answer").innerText = "Answer: " + solution
         history[historyIndex] = problem
         historyIndex++;
+        return solution;
     } catch {
         document.getElementById("answer").innerText = "Answer: Error"
     }
