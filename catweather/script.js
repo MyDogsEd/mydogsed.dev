57a52cbf134e8f87f7a9c3a6072715467e0d58f9
James <radjame52004@gmail.com>
2024-11-19 17:27:49 -0500

    Finish up catWeather project

--- catweather/script.js
+++ catweather/script.js
@@ -1,22 +1,50 @@
 async function loadWeather() {
     // Get latitude and longitude from query string
     const urlParams = new URLSearchParams(window.location.search);
-    const latitude = urlParams.get('lat');
-    const longitude = urlParams.get("long");
-    const timezone = urlParams.get("timezone")
+    const loc_id = urlParams.get("loc_id")
+
+    // If there is no latitude query string, do the default
+    if (!loc_id){
+        loc_id = "4990729" // location id for detroit
+    }
+
+
+    // make request to openmetero geo api to resolve location
+    const geoAPI = `https://geocoding-api.open-meteo.com/v1/get?id=${loc_id}`
+
+    var locationJSON = await (await fetch(geoAPI)).json()
+
+    // do the location card
+    $("#locationCardBody").append(getFlagElement(locationJSON.country_code.toLowerCase())).append(" " +locationJSON.name)
+    $("#locationCardBody")
+
+    console.log(locationJSON)
 
     // make request to weather API
     const openmeteroLink = 
-        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=${timezone}`
+        `https://api.open-meteo.com/v1/forecast?latitude=${locationJSON.latitude}&longitude=${locationJSON.longitude}&current=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=${locationJSON.timezone}`
 
-    fetch(openmeteroLink)
-        .then((response) => response.json())
-        .then(async (json) => {
-            console.log(json)
-            var image = await weathercodeImage(json.current.weather_code)
-            var imageEle = $(`<img src="${image}">`)
-            $("#temperatureContainer").append(imageEle)
-        })
+    var weatherJSON = await (await fetch(openmeteroLink)).json()
+
+    console.log(weatherJSON)
+
+    
+    // do the cat image
+    var image = await weathercodeImage(weatherJSON.current.weather_code)
+    $("#catWeatherImage").attr("src", image)
+
+    // update the cat pun
+    var pun = await getPun(weatherJSON.current.weather_code)
+    $("#catWeatherPun").text(pun)
+
+    // update the non cat pun
+    var nonPun = await getNonPun(weatherJSON.current.weather_code)
+    $("#realWeatherCondition").text(`(${nonPun})`)
+
+    // do the temperature descriptor
+    var temperatureDescriptorString = `Current temperature is ${weatherJSON.current.temperature_2m}°F`
+    $("#temperatureDescriptor").text(temperatureDescriptorString) 
+
 }
 
 async function weathercodeImage(code){ 
@@ -25,6 +53,27 @@ async function weathercodeImage(code){
         .then(json => {
             return json[code];
         })
+}
+
+async function getPun(code){
+    return fetch("./puns.json")
+        .then((result) => result.json())
+        .then(json => {
+            return json[code];
+        })
+}
+
+async function getNonPun(code){
+    return fetch("./non-puns.json")
+        .then((result) => result.json())
+        .then(json => {
+            return json[code];
+        })
+}
+
+
+function getFlagElement(countryCode){
+    return $("<img>").attr("src", `https://hatscripts.github.io/circle-flags/flags/${countryCode}.svg`).attr("width", "28")
 }
 
 loadWeather()
