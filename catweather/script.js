a1644a6f99021093746b212dd7d8af3516965c98
MyDogsEd <radjame52004@gmail.com>
2024-11-19 16:02:42 -0500

    Updated changes for catweather project

--- catweather/script.js
+++ catweather/script.js
@@ -1,4 +1,4 @@
-function loadWeather() {
+async function loadWeather() {
     // Get latitude and longitude from query string
     const urlParams = new URLSearchParams(window.location.search);
     const latitude = urlParams.get('lat');
@@ -6,8 +6,25 @@ function loadWeather() {
     const timezone = urlParams.get("timezone")
 
     // make request to weather API
-    const openmeteroLink = ""
+    const openmeteroLink = 
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=${timezone}`
 
+    fetch(openmeteroLink)
+        .then((response) => response.json())
+        .then(async (json) => {
+            console.log(json)
+            var image = await weathercodeImage(json.current.weather_code)
+            var imageEle = $(`<img src="${image}">`)
+            $("#temperatureContainer").append(imageEle)
+        })
+}
+
+async function weathercodeImage(code){ 
+    return fetch("./images.json")
+        .then((result) => result.json())
+        .then(json => {
+            return json[code];
+        })
 }
 
 loadWeather()
